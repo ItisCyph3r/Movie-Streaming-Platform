49f40beee71a9f58927f885ed89931beed095fb0
Momoh Samuel <samuelmomoh61@gmail.com>
2022-05-26 22:06:04 +0100

    beta deploy

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-// mongoose.connect(process.env.USER_SECRET)
-mongoose.connect('mongodb://localhost:27017/MovieDB')
+mongoose.connect(process.env.USER_SECRET)
+
 
 const movieSchema = new mongoose.Schema({
     id: Number,
